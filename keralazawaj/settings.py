d6970a0f45094eff66f7e09310cc27bf1cf3c82c
PV ANFAS <45325099+pvanfas@users.noreply.github.com>
2022-12-13 10:04:02 +0000

    finished

--- keralazawaj/settings.py
+++ keralazawaj/settings.py
@@ -70,16 +70,16 @@ WSGI_APPLICATION = "keralazawaj.wsgi.application"
 
 
 # Define DATABASES (postgres-production)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': config('DB_ENGINE'),
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
+    }
+}
 
 
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
